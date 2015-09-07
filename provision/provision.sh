df59633e0d77513ce594017351bb017ec6588c8e
harshadyeola <harshad.yeola@rtcamp.com>
2015-09-07 18:52:49 +0530

    added ruby installation in provision

--- provision/provision.sh
+++ provision/provision.sh
@@ -3,14 +3,26 @@
 
 function myecho()
 {
-    echo =========================================================  
+    echo =========================================================
 }
 
-sudo echo -e "[user]\n\tname = EasyEngine\n\temail = root@easyengine.com" > ~/.gitconfig 
+sudo echo -e "[user]\n\tname = EasyEngine\n\temail = root@easyengine.com" > ~/.gitconfig
 
 myecho
 
 wget -qO ee rt.cx/ee && sudo bash ee  || exit 1
+
+myecho
+# Install latest Ruby
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+\curl -sSL https://get.rvm.io | bash -s stable
+
+source /etc/profile.d/rvm.sh
+
+rvm install 2.2
+
+rvm use 2.2
 
 myecho
 # Rubygems update
